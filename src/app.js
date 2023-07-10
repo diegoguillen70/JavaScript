78060f6969ef18fd1922a5b1420b309b8cc7f940
diegoguillen70 <137721556+diegoguillen70@users.noreply.github.com>
2023-07-10 19:11:45 +0000

    commit

--- src/app.js
+++ src/app.js
@@ -4,8 +4,35 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+import { arrow } from "@popperjs/core";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let str =
+    getRadomMessage(who) +
+    " " +
+    getRadomMessage(action) +
+    " " +
+    getRadomMessage(what) +
+    " " +
+    getRadomMessage(when);
+
+  document.getElementById("excuse").innerHTML = str;
+  console.log("Hello Diego from the console!");
 };
+
+function getRadomMessage(arr) {
+  const randomIndex = Math.floor(Math.random() * arr.length);
+  return arr[randomIndex];
+}
